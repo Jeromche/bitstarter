b105c72b4a7ce1f5db2fb45203981cc67bf36fd8
Jeromche <jeroen@vandertuin.nl>
2013-07-14 21:06:25 +0000

    Fixed calling of toString method on object.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var buffer = fs.readFileSync('index.html');
-	var str = Buffer.toString(buffer);
+	var str = buffer.toString();
 
   response.send(str);
 });
