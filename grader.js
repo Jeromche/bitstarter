4c496f1fa5e018bc16c00bd228742297d53a439a
Jeromche <jeroen@vandertuin.nl>
2013-07-15 01:17:39 +0000

    Updated grader.js to take either a file or a URL as input.

--- grader.js
+++ grader.js
@@ -1,7 +1,8 @@
+
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
+Automatically grade files for the presence of specified HTML tags/atributes.
+Uses commander.js and cheerio. Teaches command line application developement
 and basic DOM parsing.
 
 References:
@@ -15,6 +16,9 @@ References:
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
+ + restler
+  - https://github.com/danwrong/restler
+
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
@@ -24,6 +28,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +49,7 @@ var loadChecks = function(checksfile) {
 	return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
+var checkHtml = function($, checksfile) {
 	var checks = loadChecks(checksfile).sort();
 	var out = {};
 	for(var ii in checks) {
@@ -65,10 +69,30 @@ if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL of HTML file')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+
+	var check = function(html, checksfile) {
+		var checkJson = checkHtml(html, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	};
+
+	// Check for file.
+	if (typeof program.url === 'undefined') {
+		check(cheerioHtmlFile(program.file), program.checks);
+		process.exit();
+	}
+
+	// Check URL.
+	restler.get(program.url)
+		.on('success', function(data) {
+			check(cheerio.load(data), program.checks);
+		})
+		.on('fail', function(data) {
+			console.error('There was a problem parsing the URL.');
+			process.exit(1);
+		});
 } else {
-	exports.checkHtmlFile = checkHtmlFile;
+	exports.checkHtml = checkHtml;
 }
